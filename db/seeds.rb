5f3f0f244f8f98d6a329a061acb252f01b4212fd
ddaawwaa <nlghtnmnt3106@gmail.com>
2019-09-30 00:11:48 +0900

    Create seeds.rb
    - APIを使って、飲食店カテゴリーをDBに保存するために作成
    - ACCESS_KEYを非表示にするため、gem'dotenv-rails'を使用

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'net/https'
+require "uri"
+require "active_support/core_ext"
+
+keyid = GRN_ACCESS_KEY
+
+data={"keyid": keyid,
+      "lang": "ja"
+      }
+
+query=data.to_query
+uri = URI("https://api.gnavi.co.jp/master/CategorySmallSearchAPI/v3/?"+query)
+
+http = Net::HTTP.new(uri.host, uri.port)
+
+http.use_ssl = true
+req = Net::HTTP::Get.new(uri)
+
+res = http.request(req)
+result = JSON.parse(res.body)
+
+categorys = result["category_s"]
+
+categorys.each do |cate|
+	Category.create(name: cate["category_s_name"], code: cate["category_s_code"])
+end
