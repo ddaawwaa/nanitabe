5891427f9056c7fe4c5c5b7e5a5011b0e5d519b5
ddaawwaa <nlghtnmnt3106@gmail.com>
2019-10-02 23:55:54 +0900

    Install bootstrap by webpack
    以下の３つをwebpackでinstall and set
    - jquery
    - popper.js
    - bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    popper: 'popper.js/dist/popper'
+  })
+)
 module.exports = environment
