b9b667a8daa98d1bf82b8a296c0d35e9c13eb5af
ddaawwaa <nlghtnmnt3106@gmail.com>
2019-10-06 23:55:53 +0900

    Create choices table

--- db/migrate/20191005083145_create_choices.rb
+++ db/migrate/20191005083145_create_choices.rb
@@ -1,7 +1,7 @@
 class CreateChoices < ActiveRecord::Migration[6.0]
   def change
     create_table :choices do |t|
-
+      t.references :category, foreign_key: true, null: false
       t.timestamps
     end
   end
