e2714e7b56541d3e163cc284ed5a21b0971919c4
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-02-07 20:33:07 +0900

    Remove choices controller and model

--- db/migrate/20191005083145_create_choices.rb
+++
@@ -1,8 +0,0 @@
-class CreateChoices < ActiveRecord::Migration[6.0]
-  def change
-    create_table :choices do |t|
-      t.references :category, foreign_key: true, null: false
-      t.timestamps
-    end
-  end
-end
