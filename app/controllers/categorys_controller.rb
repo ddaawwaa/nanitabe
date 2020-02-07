17083b757facdb6170dce5980a33361019913804
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-02-07 18:45:02 +0900

    Set validation

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -3,7 +3,14 @@ class CategorysController < ApplicationController
   end
 
   def random
-    @categorys = Category.order("RANDOM()").first(4)
+    # if flash[:random4].blank?
+      @categorys = Category.order("RANDOM()").first(4)
+      # flash[:random4] = @categorys.map{|ca| ca[:id]}
+    # else
+      
+      # @categorys = Category.where(id: flash[:random4])
+      # flash[:random4] = @categorys.map{|ca| ca[:id]}
+    # end
   end
 
   def search
@@ -13,25 +20,26 @@ class CategorysController < ApplicationController
     require "active_support/core_ext"
 
     keyid = ENV["GRN_ACCESS_KEY"]
+    # if params[:station].blank?
+      # flash.keep[:random4]
+      # redirect_to action: 'random'
+    # else
+      data={"keyid": keyid,
+            "category_s": params[:category][:name],
+            "freeword": params[:station]
+            }
+      query=data.to_query
+      uri = URI("https://api.gnavi.co.jp/RestSearchAPI/v3/?"+query)
 
-    data={"keyid": keyid,
-          "category_s": params[:category][:name],
-          "freeword": params[:station]
-          }
 
-    query=data.to_query
-    uri = URI("https://api.gnavi.co.jp/RestSearchAPI/v3/?"+query)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
 
+      req = Net::HTTP::Get.new(uri)
 
-    http = Net::HTTP.new(uri.host, uri.port)
-
-    # SSL/TLS通信を有効にする
-    http.use_ssl = true
-    # HTTPのGETリクエストを表すクラス
-    req = Net::HTTP::Get.new(uri)
-
-    res = http.request(req)
-    @results = JSON.parse(res.body)["rest"]
+      res = http.request(req)
+      @results = JSON.parse(res.body)["rest"]
+    # end
   end
 
 end
